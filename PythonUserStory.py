b8d27f3d7af8c871064e38fd48b7efcc8b737a65
anizadikian <48724834+anizadikian@users.noreply.github.com>
2019-04-14 17:41:02 -0300

    Update PythonUserStory.py

--- PythonUserStory.py
+++ PythonUserStory.py
@@ -1,14 +1,68 @@
-def setupTask():
-    task = int(input('Please, insert the task'))
-    return(task)
+import json
+def main():
 
-def setupDate():
-    date = int(input('Please, insert the deadline in the format DD/MM/YYYY'))
-    return (date)
+    with open('data.txt') as json_file:
+        data = json.load(json_file)
 
-def setupPriority():
-    priority = int(input('Please, insert the priority of the task, from 1 to 5'))
-    while priority>5 and priority<1:
-        print ("Please, insert a priority from 1 - 5")
-    else:
-        return(priority)
+    UserStory2= data["UserStory2"]
+
+    print("0: to answer new topic")
+    print("1: to see a previous answer")
+    print("2: to answer again a question")
+
+    user_choice = int(input(""));
+
+    if user_choice == 0:
+        user_question = input("please insert a task.")
+
+        data['UserStory2'].append({
+            'number': 1,
+            'task':user_task,
+            'answer': ""
+        })
+
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
+
+    elif user_choice == 1:
+
+        task_answer = data['UserStory2']
+
+
+        for object in questions_answer:
+            print(object)
+
+
+    elif user_choice == 2:
+
+        task_answer = data['UserStory2']
+
+
+        for object in questions_answer:
+            print(object)
+
+
+        number_of_tasks = input ("enter the number of tasks you want to accomplish?")
+        task_answer = input("Please enter the ask you want to accomplish")
+        answer_to_question = input("Enter the name of the task.")
+        deadline_question = input("Enter the deadline of the task")
+        time_question = input("Enter the time it will take to accomplish it")
+        answer = {'Number': number_of_tasks,
+                  'Task': task_answer,
+                  'Answer': answer_to_question,
+                  'Deadline': deadline_question,
+                  'Time': time_question}
+
+
+        data['UserStory2'].remove(
+            questions_answer[int(number_of_task) - 1]
+        )
+
+        data['UserStory2'].append({
+            'number': int(next(iter(object.values()))),
+            'question': question_to_answer,
+            'answer': answer_to_question
+        })
+
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
