1951d86591eeadbc8ffb5f3135bd4dfdf0147e96
knargocer <48725959+knargocer@users.noreply.github.com>
2019-05-06 12:23:06 +0400

    Update PythonUserStory.py

--- PythonUserStory.py
+++ PythonUserStory.py
@@ -1,38 +1,46 @@
 import json
+
 print("This is your ToDo list helper. It will help you organize your tasks. Please fill in the information bellow")
 
+
 def new_task():
-    with open("DataJson.json") as data:
-        data_dict = json.load(data)
-        data.write(json.dumps(data_dict))
+    # with open("DataJson.json") as data:
+    #     data_dict = json.load(data)
+        x=input("kdhkfh")
+        #data.write(json.dumps(data_dict))
+        with open('DataJson.json', 'w') as f:
+            json.dump(x, f)
+
+
 
 def task_modifaction():
     with open("DataJson.json") as data:
-            data_dict = json.load(data)
-            if (data_dict):
-                print("File exists:")
-            task_answer = input("Please enter the task you want to modify")
-            json_load['tasks'][task_answer]['deadline'] = input("Enter the deadline of the task")
-            json_load['tasks'][task_answer]['Time'] = input("Enter the time it will take to accomplish it")
-            new_answer = {'Task': task_answer,
-                      'Deadline': deadline_question,
-                      'Time':  time_question}
+        data_dict = json.load(data)
+        if (data_dict):
+            print("File exists:")
+        task_answer = input("Please enter the task you want to modify")
+        data_dict['tasks'][task_answer]['deadline'] = input("Enter the deadline of the task")
+        data_dict['tasks'][task_answer]['Time'] = input("Enter the time it will take to accomplish it")
+        new_answer = {'Task': task_answer,
+                      'Deadline': data_dict['tasks'][task_answer]['deadline'],
+                      'Time': data_dict['tasks'][task_answer]['Time']}
 
-            data['DataJson.json'].remove(
-                task_answer[int(tasks) - 1]
-            )
+        data['DataJson.json'].remove(
+            task_answer[int(tasks) - 1]
+        )
 
-            data['DataJson.json'].append({
-                'Task': int(next(iter(object.values()))),
-                'Deadline': deadline_question,
-                'Time': time_question
-            })
+        data['DataJson.json'].append({
+            'Task': int(next(iter(object.values()))),
+            'Deadline': deadline_question,
+            'Time': time_question
+        })
 
-            with open('data.json', 'w') as outfile:
-                outfile.write(json.dump(data, outfile))
-                else:
-                print("file does not exist")
-                return user_data
+        with open('data.json', 'w') as outfile:
+            outfile.write(json.dump(data, outfile))
+        else:
+            print("file does not exist")
+            return user_data
+
 
 def see_all_tasks():
     with open("DataJson.json") as data:
@@ -43,11 +51,10 @@ def see_all_tasks():
 
 
 def main():
-
     with open('data.txt') as DataJson:
         data_dict = json.load(DataJson.json)
 
-    DataJson= data["DataJson.json"]
+    DataJson = data["DataJson.json"]
 
     print("0: To fill in a new task")
     print("1: To see a all tasks")
@@ -57,7 +64,6 @@ def main():
 
     if user_choice == 0:
         user_question = input("Please insert a new task")
-
 
         data['DataJson.json'].append({
             "Task1": "",
@@ -78,9 +84,10 @@ def main():
         task_answer = data['DataJson']
 
         return task_modifaction()
-        
-    else :
+
+    else:
         print("Please type 0, 1 or 2")
         exit
-        
+
+
 main()
